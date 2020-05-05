ac2c3b12a179b0a44acc0234c7dabc17f257e99b
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-05 18:04:01 -0500

    basic scaffolding

---
+++ Lab2ATM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
