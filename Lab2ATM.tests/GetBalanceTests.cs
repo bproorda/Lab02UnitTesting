a8382942358f0cb0f5fcc236af94618bc3868e2b
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-05 20:19:19 -0500

    added two tests for getBalance method

---
+++ Lab2ATM.tests/GetBalanceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lab2ATM.tests
+{
+   public class GetBalanceTests
+    {
+        [Fact]
+        public void BalanceCheck()
+        {
+            double result = ATM.GetBalance();
+            double expected = Globals.BalanceAmount;
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ChangingBalanceCheck()
+        {
+            double intial = ATM.GetBalance();
+            Globals.BalanceAmount += 100;
+            double result = ATM.GetBalance();
+            double expected = intial + 100;
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
