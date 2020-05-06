570af2f2fa1e64183bf7c9659e019e66ab11f6b8
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-05 19:45:45 -0500

    added view balance method, not sure how to test it

--- Lab2ATM/Program.cs
+++ Lab2ATM/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Lab2ATM
 {
-    class Program
+      public class Globals
     {
+        public static decimal BalanceAmount = 0;
+    }
+    class Program
+    {  
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(GetBalance());
+            Console.ReadLine();
+        }
+        public static string GetBalance()
+        {
+            string message = String.Format("Your current balance = {0:C2}", Globals.BalanceAmount);
+            return message;
         }
     }
 }
